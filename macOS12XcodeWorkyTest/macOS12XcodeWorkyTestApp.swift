6108b0b246fc98359a6c8e927eee08b7e0725e31
Jonathan Hume <jonathan.hume@gmail.com>
2021-10-10 11:25:48 +0100

    Initial Commit

---
+++ macOS12XcodeWorkyTest/macOS12XcodeWorkyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS12XcodeWorkyTestApp.swift
+//  macOS12XcodeWorkyTest
+//
+//  Created by Jonathan Hume on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct macOS12XcodeWorkyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
